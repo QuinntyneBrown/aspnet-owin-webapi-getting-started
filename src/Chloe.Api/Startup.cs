1b938c69693cf3e39cfeae27f84190d86acec005
Quinntyne Brown <Quinntyne.Brown@shawmedia.ca>
2016-04-06 14:18:20 -0400

    initial commit

---
+++ src/Chloe.Api/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Chloe.Api.Startup))]
+
+namespace Chloe.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
---
+++ src/Chloe.SelfHost/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace Chloe.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9006/";
+
+            WebApp.Start<Chloe.Api.Startup>(url: baseAddress);
+
+            Console.ReadLine();
+        }
+    }
+}
